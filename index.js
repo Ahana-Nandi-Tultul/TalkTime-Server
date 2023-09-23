6a285d97a1bc3ce6fc117fd8ffbbf5447ac4efea
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-09-24 03:33:41 +0600

    add 'JWT' and verify it

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+var jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: {status: true, message: 'unauthorized access'}});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decode) => {
+    if(error){
+      return res.status(401).send({error: {status: true, message: 'unauthorized access'}});
+    }
+    req.decode = decode;
+    next();
+  })
+}
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epxwefd.mongodb.net/?retryWrites=true&w=majority`;
@@ -19,6 +37,14 @@ const client = new MongoClient(uri, {
   }
 });
 
+app.post('/jwt', (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'})
+    res.send({token})
+})
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,6 +54,10 @@ async function run() {
 
 
     // user related api
+    app.get('/users',verifyJwt, async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/users', async(req, res) => {
         const newUser = req.body;
         const result = await userCollection.insertOne(newUser);
