22ec533840f27743f4eb70056a7a5f68e3fb7f2c
Ahana Nandi <ahananondi6@gmail.com>
2023-10-11 09:46:59 +0600

    add cors headers

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://talktime-2e686.web.app/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 const verifyJwt = (req, res, next) => {
   const authorization = req.headers.authorization;
